447a3581f2c474c38b8e88977976f892b997f6b7
shaik.k.a <sha_ikin@yahoo.co.in>
2015-12-04 18:03:12 +0530

    testng test class included.

---
+++ ScreenRecorder/src/com/proj/screenrecorder/callee/ATU_Screenrecorder_Calle.java
@@ -0,0 +1,32 @@
+package com.proj.screenrecorder.callee;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.Test;
+
+import com.proj.screenrecorder.ScreenRecorder_ATU;
+
+public class ATU_Screenrecorder_Calle {
+
+	@Test
+	public static void TestScreenrecorder() throws Exception {
+		
+		ScreenRecorder_ATU.startRecorder();
+		
+		FirefoxDriver driver=new FirefoxDriver();
+		
+		driver.get("https://qa.haicpm.com/");
+		
+		driver.manage().timeouts().pageLoadTimeout(60, TimeUnit.SECONDS);
+		
+		driver.findElement(By.id("strUserLogin")).sendKeys("snallamothu@hostanalytics.com");
+		driver.findElement(By.id("strUserPwd")).sendKeys("satish$123456");
+		driver.findElement(By.id("btnLogin")).click();
+		
+		driver.quit();
+		ScreenRecorder_ATU.stopRecorder();
+	}
+
+}
